3ffd09c4a71772bfbb6f18d7dadafe51aec40847
Andrew <sl4shed@proton.me>
2026-02-27 22:41:24 +0200

    i dont wanna be dismal

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,26 @@
-<template>
-  <div>
-    <Window v-for="(win, i) in windows"
-            :key="win.id"
-            :title="win.title"
-            :style="{ zIndex: i }"
-            :active="win === activeWindow"
-            @close="closeWindow(win)"
-            @focus="focusWindow(win)"/>
-    <button @click="windows.push({ id: Date.now(), title: 'New Window' })">Open</button>
-  </div>
-</template>
-
 <script>
-
-
+import Wallpaper from "./assets/wallpaper.jpg"
+import Folder from "./assets/folder.png"
 import Window from "./components/Window.vue"
+import Icon from "./components/Icon.vue"
 
 export default {
-  components: { Window },
+  components: { Window, Icon },
   provide() {
     return { windows: this.windows }
   },
   data() {
     return {
       windows: [{ id: Date.now(), title: 'First Window' }],
-      activeWindow: null
+      activeWindow: null,
+      Folder
     }
   },
   methods: {
+    openWindow(win) {
+      this.windows.push({ id: Date.now(), title: 'New Window' })
+    },
+
     focusWindow(win) {
       this.activeWindow = win
       const i = this.windows.indexOf(win)
@@ -42,3 +35,38 @@ export default {
   }
 }
 </script>
+
+<template>
+  <div class="windows">
+    <Window v-for="(win, i) in windows"
+            :key="win.id"
+            :title="win.title"
+            :style="{ zIndex: i }"
+            :active="win === activeWindow"
+            @close="closeWindow(win)"
+            @focus="focusWindow(win)"/>
+  </div>
+
+  <div class="icons">
+    <Icon name="pepe" :icon="Folder" :callback="openWindow"></Icon>
+  </div>
+</template>
+
+<style lang="scss">
+html, body {
+  margin: 0;
+  padding: 0;
+}
+
+.windows {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+
+  background-image: url('./assets/wallpaper.jpg');
+}
+</style>
---
+++ src/components/Icon.vue
@@ -0,0 +1,69 @@
+<script>
+export default {
+  name: "Icon",
+  props: {
+    icon: {
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    callback: {
+      type: Function,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="icon">
+    <img :src="icon"  :alt="name" />
+    <p>{{ name }}</p>
+  </div>
+</template>
+
+<style scoped>
+.icon {
+  color: white;
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  width: 80px;
+  height: 75px;
+  max-width: 80px;
+  max-height: 75px;
+  min-width: 80px;
+  min-height: 75px;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  user-select: none;
+
+  border: solid 1px transparent;
+  z-index: 0;
+  gap: 4px;
+}
+
+.icon img {
+  user-select: none;
+  width: 48px;
+  height: 48px;
+}
+
+.icon p {
+  user-select: none;
+  font-family: "Segoe UI", sans-serif;
+  font-size: 11px;
+  text-shadow: 0 1px 3px #000, 0 0 4px #000;
+  text-align: center;
+}
+
+.icon:hover {
+  background-color: #3399ff66;
+  border: 1px solid #3399ffaa;
+  border-radius: 3px;
+}
+</style>
